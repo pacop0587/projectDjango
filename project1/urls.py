3a6986ad22e7fede80a3ec364099257fc2a1d16f
Paco <pacop0587@gmail.com>
2025-08-03 22:39:06 -0600

    Add feature project school, login and test

--- project1/urls.py
+++ project1/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from school import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/')),
+    path('login/', views.loginUser, name='login'),
     path('admin/', admin.site.urls),
+    path('index/', views.indexHome, name='index'),
 ]
---
+++ school/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_exists = User.objects.filter(username=username).exists()
+
+        if not user_exists:
+            return render(request,
+                  'login.html',
+                  {'form': AuthenticationForm,
+                   'error': 'El usuario no existe'})
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request,
+                  'login.html',
+                  {'form': AuthenticationForm,
+                   'error': 'Usuario y/o contraseña incorrecta'})
+
+    return render(request,
+                  'login.html',
+                  {'form': AuthenticationForm,
+                   'error':''})
+        
+
+def indexHome(request):
+    return render(request,'index.html')
